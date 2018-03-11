f6e6ced237366b1661f2ccd1170548df0eb0830c
Song Zhao <szhao@snares-03.cs.wisc.edu>
2018-03-10 19:51:06 -0600

    s

--- bayes.py
+++ bayes.py
@@ -32,7 +32,6 @@ def printNB(metadata):
     print
 
 def buildNaiveBayesNet(X, Y, numVals):
-    # compute the base rate of Y & P(X = X_i|Y) for all i
     P_Y = basicP(Y, 2)
     P_XgY = computeP_XgY(X, Y, numVals)
     return P_Y, P_XgY
@@ -49,31 +48,18 @@ def basicP(target, possible_value):
     return dist
 
 def computeP_XgY(X, Y, numVals):
-    '''
-    compute P(X = X_i | Y = Y_j), for all i and j
-    '''
+
     P_XgY = [[],[]]
-    # loop over all values of Y (binary)
     for val in range(0,2):
-        #X_sub, Y_sub = getDataSubset(X, Y, np.shape(X)[1], val)
         resize = Y == val
         X_resize = X[resize, :]
-        print X_resize
         for n in range(np.shape(X)[1]):
             P_XgY[val].append(basicP(X_resize[:,n], numVals[n]))
     return P_XgY
 
-def getDataSubset(X, Y, idx_feature, val_feature):
-    idx_target = Y == val_feature
-
-    X_reduced = X[idx_target,:]
-    Y_reduced = Y[idx_target]
-
-    return X_reduced, Y_reduced
-
-def computePredictions_NaiveBayes(X_test, P_Y, P_XgY):
-    Y_hat, Y_prob = np.zeros(np.shape(X_test)[0]), np.zeros(np.shape(X_test)[0])
-    # loop over rows, make prediction
+def computePredictions_NaiveBayes(X_test, P_Y, P_XgY, metadata, metarange):
+    Y_hits = []
+    Y_p = []
     for m in range(np.shape(X_test)[0]):
         probs = np.zeros(2)
         for y_val in range(2):
@@ -81,29 +67,39 @@ def computePredictions_NaiveBayes(X_test, P_Y, P_XgY):
             for n in range(len(X_test[m,:])):
                 temp = P_XgY[y_val][n]
                 probs[y_val] *= temp[X_test[m,:][n]]
-        # get the idx and the value of the max
         prediction_distribution = np.divide(probs, np.sum(probs))
         predictedClass = np.argmax(prediction_distribution)
         predictedProbability = prediction_distribution[predictedClass]
-        Y_hat[m] = np.argmax(prediction_distribution)
-        Y_prob[m] = prediction_distribution[predictedClass]
-    return Y_hat, Y_prob
-
-def printTestResults(Y_hat, Y_prob, Y_test, metadata):
+        Y_hits.append(np.argmax(prediction_distribution))
+        Y_p.append(prediction_distribution[predictedClass])
     y_range = metadata[metadata.names()[-1]][1]
-    for m in range(len(Y_test)):
-        prediction = y_range[int(Y_hat[m])]
-        truth = y_range[Y_test[m]]
-        print('%s %s %.12f' % (prediction.strip('"\''), truth.strip('"\''), Y_prob[m]))
-    hits = np.sum(np.around(Y_hat) == Y_test)
-    print ('\n%d' % hits)
+    hit = 0
+    for m in range(len(metarange)):
+        prediction = y_range[Y_hits[m]]
+        truth = y_range[metarange[m]]
+        if(Y_hits[m] == metarange[m]):
+            hit = hit + 1
+        print('%s %s %.12f' % (prediction, truth, Y_p[m]))
+    print ('\n%d' % hit)
+    return Y_hits, Y_p
+
+##TAN
+def getWeight(feature, feature_range):
+    return
 
 args = [arg for arg in sys.argv]
 feature, class_res, metadata, feature_range = loadData(args[1])
 test_f, test_c, test_m, test_r = loadData(args[2])
 printNB(metadata)
 
+
 py, p_xny = buildNaiveBayesNet(feature, class_res, feature_range)
-y_h, y_p = computePredictions_NaiveBayes(test_f, py, p_xny)
-printTestResults(y_h, y_p, test_c, metadata)
+y_h, y_p = computePredictions_NaiveBayes(test_f, py, p_xny, metadata,test_c)
+print p_xny
+print feature
+print " class_res"
+print class_res
+
+
+
 
